4f593934ea82264841cb7b742ff9ccfd8bd0660f
codeher-rahavi <rahaviganeshan529@gmail.com>
2025-05-05 09:48:33 +0530

    problem sheets

---
+++ 24 hour format to 12 hour format.py
@@ -0,0 +1,10 @@
+hr,mins=map(int,input().split(':'))
+if hr==00 and mins==00:
+    print("12:00 AM")
+elif hr==12 and mins==00:
+    print("12:00 PM")
+elif hr>12:
+    print(f"{hr-12}:{mins:02d} PM")
+elif hr<12 :
+    print(f"{hr} : {mins:02d} AM")
+
