f2efd435635b22a0a34da6f267c6fd4c054395af
codeher-rahavi <rahaviganeshan529@gmail.com>
2025-05-08 16:03:58 +0530

    python practice

---
+++ rounding upto two units in a float.py
@@ -0,0 +1,14 @@
+'''
+a=int(input())
+ans=0
+ans=a*3.1456
+print(round(ans,2))
+
+
+or...we can use format() and use the variable_name:.number_of_units_to_round f
+'''
+
+a=int(input())
+ans=0
+ans=a*3.1456
+print(f'{ans:.2f}')
